3d4b005667895929ee5c4c47615e05a369dee344
Fausto <fausto@thexs.ca>
2020-06-05 11:21:52 -0400

    VxSlider changes

--- vx-slider.js
+++ vx-slider.js
@@ -1,9 +1,9 @@
 export default {
   template: `<!--  -->
 <v-layout>
-  <v-slider class="align-center mr-3" :value="value" :disabled="disabled" :min="min" :max="max" :label="label" @input="$emit('input', $event)">
+  <v-slider class="align-center mr-3" :value="value" :disabled="disabled" :min="min" :max="max" :label="label" @input="$emit('input', $event)" hide-details>
     <template v-slot:append>
-      <v-text-field class="mt-0 pt-0" :value="value" :disabled="disabled" :min="min" :max="max"  type="number" @input="$emit('input', $event)">
+      <v-text-field class="mt-0 pt-0" :value="value" :disabled="disabled" :min="min" :max="max"  type="number" @input="$emit('input', $event)" hide-details>
       </v-text-field>
     </template>
   </v-slider>
